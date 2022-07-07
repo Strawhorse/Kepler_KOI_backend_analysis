bd6fb7f3e7cb39d5c1c92abdd25a1d70955ed427
Strawhorse <strawhorse@gmail.com>
2022-07-07 17:18:24 +0100

    turned createReadStream into a function and downloaded another list of similar exoplanets to compare to

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const fs = require('fs')
 
 
 const habitablePlanet = []
+const exoPlanetList = []
 
 const isHabitablePlanet = (planet) => {
-    if (planet['koi_disposition'] === 'CONFIRMED') {
+    if (planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6) {
         return planet
     }
 }
 
 // will create event emitter
-fs.createReadStream('./kepler_data.csv')
+const checkKeplerPlanets = () => {
+    fs.createReadStream('./kepler_data.csv')
     // pipe connects a readable stream source to a writeable stream destination which takes in data
     .pipe(parse({
         comment: "#",
@@ -25,7 +29,35 @@ fs.createReadStream('./kepler_data.csv')
         console.log(err)
     })
     .on('end', () => {
-        console.log(habitablePlanet)
-        console.log("Done parsing Kepler data")
+        console.log(habitablePlanet.map((planet) => {
+            return planet['kepler_name']
+        }))
+        console.log("Done parsing Kepler data \n")
+        console.log(`The total number of habitable planets found is: ${habitablePlanet.length}`)
 })
+}
 
+
+const comparePlanets = () => {
+    fs.createReadStream('./exoplanet.eu_catalog.csv')
+    .pipe(parse({
+        // comment: "#",
+        columns: true
+    }))
+    .on('data', (data) => {
+        exoPlanetList.push(data)
+    })
+    .on('error', (err) => {
+        console.log(err)
+    })
+    .on('end', () => {
+        console.log(exoPlanetList.map((exo) => {
+            return exo['name']
+            // create a loop to check if any of the Kepler exoplanets are in the 
+            // other exoplanet list
+        }))
+    })
+}
+
+checkKeplerPlanets()
+comparePlanets()
